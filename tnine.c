4dfe09a6dd6485f129e4c3821d114df5fc40a51b
up201805327 <up201805327@alunos.fc.up.pt>
2021-05-13 16:50:58 +0100

    Changes in the layout

--- tnine.c
+++ tnine.c
@@ -12,6 +12,7 @@
 #include "header.h"
 
 GtkWidget *window;
+GtkWidget *fixed;
 GtkWidget *grid;
 GtkWidget *button;
 GtkWidget *label;
@@ -19,10 +20,8 @@ GtkWidget *label;
 void button_clicked(GtkWidget *widget, gpointer data) {
     g_print("%s\n", (char*) data);
     char * aux = malloc(256*sizeof(char)); 
-    //const char * str = (char *) gtk_label_get_text((GtkLabel *) label);
-    //strcat(aux,str);
-    //strcat(aux,(char*)data);
-    gtk_label_set_text((GtkLabel *)label,(char *) data);
+    char * str = (char *) gtk_label_get_text((GtkLabel *) label);
+    gtk_label_set_text((GtkLabel *)label,strcat(str, (char *) data));
 }
 
 
@@ -33,28 +32,28 @@ int main(int argc, char *argv[]) {
     gtk_init(&argc, &argv);
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-    gtk_window_set_default_size(GTK_WINDOW(window), 300, 0);
+    gtk_window_set_default_size(GTK_WINDOW(window),0,0);   // tamanho da janela
     gtk_window_set_title(GTK_WINDOW(window), "T9");
     gtk_container_set_border_width(GTK_CONTAINER(window), 5);
 
+    fixed = gtk_fixed_new();
+    gtk_container_add(GTK_CONTAINER(window), fixed);
+
     label = gtk_label_new("label");
+    gtk_fixed_put(GTK_FIXED(fixed),label,5,5);   // posição da label
 
     grid = gtk_grid_new();
-    gtk_grid_set_row_spacing(GTK_GRID(grid), 2);
-    int i = 0;
-    int j = 0;
+    gtk_grid_set_row_spacing(GTK_GRID(grid), 2);    
     int pos = 0;
-    gtk_grid_attach(GTK_GRID(grid), label, 0, 0, 3, 1);
-
-    for (i=1; i < 5; i++) {
-        for (j=0; j < 3; j++) {
+    for (int i=0; i < 4; i++) {
+        for (int j=0; j < 3; j++) {
             button = gtk_button_new_with_label(values[pos]);
             gtk_grid_attach(GTK_GRID(grid), button, j, i, 1, 1);
             g_signal_connect(G_OBJECT(button), "clicked",G_CALLBACK(button_clicked),values[pos]);
             pos++;
         }
     }
-    gtk_container_add(GTK_CONTAINER(window), grid);
+    gtk_fixed_put(GTK_FIXED(fixed),grid,5,27);   // posição da tabela
     
     g_signal_connect(G_OBJECT(window), "destroy",G_CALLBACK(gtk_main_quit), NULL);
     gtk_widget_show_all(window);
